63e40183f904fce18b3382cea4ca93629c9e4ccd
Vektorweg <vektorweg@live.de>
2012-12-10 17:13:58 +0100

    working files

--- content/marius/gen.sh
+++ content/marius/gen.sh
@@ -1,6 +1,6 @@
 
-#pandoc -t beamer slides.mdml -o slides.pdf
+# pandoc -t beamer slides.mdml -o slides.pdf
 
-pandoc -t latex slides.mdml -o pandocmarkdown.tex
+pandoc -t beamer slides.mdml -o pandocmarkdown.tex
 
 # depends on Pandoc, Latex (and Latex-Beamer)
